8457c2740ea8789223f0ef2392232b5bc5326912
ASKhem <khemwirtz@gmail.com>
2024-05-11 17:53:31 +0200

    configurar para que se visualice verticalmente

--- pcall-web/components/main/buildPC/ProgressBar.jsx
+++ pcall-web/components/main/buildPC/ProgressBar.jsx
@@ -1,11 +1,11 @@
-export default function ProgressBar({progress}) {
+export default function ProgressBar({ progress }) {
   const progressPercentage = (progress / 12) * 100;
 
   return (
-    <div className="relative w-9/12 h-5 bg-zinc-300 border-2 border-zinc-400 rounded-full shadow-xl shadow-black flex items-center justify-start text-white transition-all duration-300 hover:text-orange-600">
-      <div className={`h-full text-sm flex items-center justify-center bg-custom-blue rounded-full w-[${progressPercentage}%]`}>
-        {progress !== 0 ? `${progress}/12` : <div className ='w-full flex items-center justify-center'>0/12</div>}
+    <div className="relative w-6 h-full bg-zinc-300 border-2 border-zinc-400 rounded-sm shadow-xl shadow-zinc-600 flex items-end justify-center text-white transition-all duration-300 hover:text-orange-600">
+      <div className="w-full text-sm flex items-center justify-center bg-custom-blue rounded-sm" style={{ height: `${progressPercentage}%` }}>
+        
       </div>
     </div>
-  )
+  );
 }
